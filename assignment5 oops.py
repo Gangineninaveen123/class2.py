f9123d8f59685853f7e7de8be42f6c5251114ca8
G Naveen <93296382+Gangineninaveen123@users.noreply.github.com>
2021-12-15 16:48:55 +0530

    Add files via upload

---
+++ assignment5 oops.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class power():
+    def pow(self,x,n):
+        return x**n
+    
+obj1 = power()
+print('sample_input: ')
+
+x = int(input('x: '))
+
+n = int(input('n: '))
+
+
+print('sample_output: ')
+print(obj1.pow(x,n))
+
+
+# In[ ]:
+
+
+
+
